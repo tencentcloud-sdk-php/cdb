0657df997722be6886b172432cbfef00c555a2f7
tencentcloudapi <tencentcloudapi@tencent.com>
2024-11-12 14:38:40 +0800

    release 3.0.1260

--- src/TencentCloud/Cdb/V20170320/Models/ModifyDBInstanceReadOnlyStatusResponse.php
+++ src/TencentCloud/Cdb/V20170320/Models/ModifyDBInstanceReadOnlyStatusResponse.php
@@ -18,27 +18,19 @@ namespace TencentCloud\Cdb\V20170320\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * SubmitInstanceUpgradeCheckJob返回参数结构体
+ * ModifyDBInstanceReadOnlyStatus返回参数结构体
  *
- * @method integer getJobId() 获取任务 ID
- * @method void setJobId(integer $JobId) 设置任务 ID
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class SubmitInstanceUpgradeCheckJobResponse extends AbstractModel
+class ModifyDBInstanceReadOnlyStatusResponse extends AbstractModel
 {
-    /**
-     * @var integer 任务 ID
-     */
-    public $JobId;
-
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
-     * @param integer $JobId 任务 ID
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -54,10 +46,6 @@ class SubmitInstanceUpgradeCheckJobResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("JobId",$param) and $param["JobId"] !== null) {
-            $this->JobId = $param["JobId"];
-        }
-
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
